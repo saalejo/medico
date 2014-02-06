e743b861f2ee486072621e9dcd285f082e7b3ac3
saalejo <bermudezdiegoalejandro@gmail.com>
2014-02-06 07:31:38 -0500

    servicio

---
+++ medicoAdmiciones/src/admisiones/db/dto/modelo/Servicio.java
@@ -0,0 +1,51 @@
+package admisiones.db.dto.modelo;
+
+import java.io.Serializable;
+
+public class Servicio implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6391334089818072446L;
+	int id;
+	String codigo;
+	String descripcion;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the codigo
+	 */
+	public String getCodigo() {
+		return codigo;
+	}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	/**
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+	/**
+	 * @param descripcion the descripcion to set
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
