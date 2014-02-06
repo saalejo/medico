e8960acae5c70aeb8429658e9cde2fbf082e600d
saalejo <bermudezdiegoalejandro@gmail.com>
2014-02-06 07:34:39 -0500

    Pabellon

---
+++ medicoAdmiciones/src/admisiones/db/dto/modelo/Pabellon.java
@@ -0,0 +1,54 @@
+package admisiones.db.dto.modelo;
+
+import java.io.Serializable;
+
+public class Pabellon implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7412418101264256453L;	
+	int id;
+	Servicio servicio;
+	Sede sede;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the servicio
+	 */
+	public Servicio getServicio() {
+		return servicio;
+	}
+	/**
+	 * @param servicio the servicio to set
+	 */
+	public void setServicio(Servicio servicio) {
+		this.servicio = servicio;
+	}
+	/**
+	 * @return the sede
+	 */
+	public Sede getSede() {
+		return sede;
+	}
+	/**
+	 * @param sede the sede to set
+	 */
+	public void setSede(Sede sede) {
+		this.sede = sede;
+	}
+	
+	
+	
+
+}
